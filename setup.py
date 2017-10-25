adf6c071e1ad3368013e3d12f3d4b25e067efd5c
Matt Wisner <matt@detroitweb.company>
2017-10-25 13:44:36 -0400

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='python_kong_management',
-    version='0.2.0',
+    version='0.2.1',
     description="Python api wrapper for Kong API Gatway",
     long_description=readme + '\n\n' + history,
     author="Matt Wisner",
