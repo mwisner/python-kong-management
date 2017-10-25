8ca1a0a099e9de7da3fca0671ed77d1f23885f61
Matt Wisner <matt@detroitweb.company>
2017-10-25 15:19:40 -0400

    Another test

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author="Matt Wisner",
     author_email='mwisner@fourthlab.com',
     url='https://github.com/mwisner/python-kong-management',
-    packages=find_packages(include=['kong']),
+    packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
     license="MIT license",
